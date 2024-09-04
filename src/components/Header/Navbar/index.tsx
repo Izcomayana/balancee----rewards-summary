e10b81f81d2c51efdc89978967ae475dffe1514f
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-09-04 02:58:24 +0100

    off-canvas

--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
 import balancee from "../../../../public/balancee-logo.png";
 
 const Navbar = () => {
@@ -17,7 +16,9 @@ const Navbar = () => {
           className="text-black"
           onClick={() => setIsOpen(!isOpen)}
         >
-          <GiHamburgerMenu />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-8">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+          </svg>
         </button>
         </div>
 
@@ -28,13 +29,27 @@ const Navbar = () => {
           onClick={() => setIsOpen(false)}
         ></div>
         <div
-          className={`fixed top-0 left-0 w-64 h-full bg-white shadow-lg transform transition-transform duration-300 ${
+          className={`fixed top-0 left-0 w-[70%] h-full bg-white shadow-lg transform transition-transform duration-300 md:w-[55%] ${
             isOpen ? "translate-x-0" : "-translate-x-full"
           }`}
         >
-          <div className="p-4">
-            <h2 className="text-xl font-bold">Menu</h2>
-            <ul className="mt-4">
+          <div className="p-8">
+          <div className="flex justify-between items-center">
+          <div>
+            <img src={balancee} alt="balancee-logo" className="w-28" />
+          </div>
+          
+          <button
+          className="text-black"
+          onClick={() => setIsOpen(false)}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+</svg>
+
+        </button>
+        </div>
+            <ul className="mt-24">
               <li className="mb-2">
                 <a href="#" className="text-gray-700">
                   Home
