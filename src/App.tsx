01bc41956cd8e4b92d52fc34f079fa1d016f0281
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-09-07 13:41:58 +0100

    scroll bar

--- src/App.tsx
+++ src/App.tsx
@@ -36,26 +36,3 @@ function App() {
 }
 
 export default App;
-
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Home from "@/pages/Home";
-// import History from "./pages/History";
-// import Header from "./components/Header";
-// import Cashout from "./pages/Cashout";
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="">
-//         <Header />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/history" element={<History />} />
-//           <Route path="/cashout" element={<Cashout />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
